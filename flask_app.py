898c682e35dbf34a02750ad05bf310ac723a1da3
Linda <kucunlinda@gmail.com>
2025-11-13 20:11:18 +0100

    own changes

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,11 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
-    @app.route('/')
-    def home():
-        return "<p>Hello world!</p>"
+    app.run(port=5000, debug=True)
+
